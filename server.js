67f355b9da4b2455fd852311362edd496a217098
DenysShchypt <denysshchyptsovit@gmail.com>
2023-12-12 16:03:05 +0200

    process.env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 const mongoose=require("mongoose");
-// const {DB_HOST, PORT = 3000}=process.env;
-const DB_HOST = "mongodb+srv://Denys:d2KR7fvS9hQhV2nU@cluster0.vj6xgqm.mongodb.net/db-contacts"
+const {DB_HOST, PORT = 3000}=process.env;
+// const DB_HOST = "mongodb+srv://Denys:d2KR7fvS9hQhV2nU@cluster0.vj6xgqm.mongodb.net/db-contacts"
 const app = require('./app');
+mongoose.set("strictQuery",true);
 
-mongoose.connect(DB_HOST).then(app.listen(3000, () => {
+mongoose.connect(DB_HOST).then(app.listen(PORT, () => {
   console.log("Database connection successful")
 })).catch(error=>{
   console.log(error.message);
