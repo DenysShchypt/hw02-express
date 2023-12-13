6f47e6f357ab14ed2c35717f5e41c71bc7584a13
DenysShchypt <denysshchyptsovit@gmail.com>
2023-12-13 16:59:34 +0200

    change server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 const mongoose = require("mongoose");
 const app = require('./app');
-const {DB_HOST, PORT = 3000}=process.env;
+// const {DB_HOST, PORT = 3000}=process.env;
 // const {Contact}=require('./models/contact')
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
+const DB_HOST="mongodb+srv://Denys:rqwslprF0ZWrAgdF@cluster0.mstgf4x.mongodb.net/db-contacts?retryWrites=true&w=majority"
 
-mongoose.connect(DB_HOST).then(app.listen(PORT, () => {
+mongoose.connect(DB_HOST).then(app.listen(3000, () => {
   // const result = Contact.find();
   // console.log(result);
 
