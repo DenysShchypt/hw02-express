e04a0d5b0b2367f458db2b4aa1311d6c724772d4
DenysShchypt <denysshchyptsovit@gmail.com>
2023-12-15 18:14:34 +0200

    change schema

--- models/contact.js
+++ models/contact.js
@@ -31,7 +31,7 @@ const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean().required().messages({'any.required': `missing field favorite` })
+  favorite: Joi.boolean().required()
 });
 
 // Схема обробки PATCH запиту
